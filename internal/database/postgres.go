965a31f9039dd420969263181976c1d0323d85e1
arpitmandhotra <arpit2003m@gmail.com>
2026-06-11 15:22:43 +0530

    feat: applied cloud environment variables for databases

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -2,27 +2,31 @@ package database
 
 import (
 	"log"
-
+     "os"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"github.com/arpitmandhotra/api-integrator/internal/domain"
 )
 
-// NewPostgresClient creates and returns a connected Postgres database engine
 func NewPostgresClient() *gorm.DB {
-	// This DSN (Data Source Name) matches the exact credentials you gave Docker
-	dsn := "host=host.docker.internal user=kotman password=secret dbname=kotman_db port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	// 1. Try to fetch the secure Neon URL from AWS
+	dsn := os.Getenv("DATABASE_URL")
 	
+	// 2. If it's empty, use your exact local Kotman configuration
+	if dsn == "" {
+		dsn = "host=host.docker.internal user=kotman password=secret dbname=kotman_db port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to Postgres Vault: %v", err)
 	}
 
 	log.Println("--> Successfully connected to PostgreSQL Cold Storage!")
-	// Tell GORM to build the tables based on our structs!
 	log.Println("--> Running PostgreSQL Auto-Migration...")
 	if err := db.AutoMigrate(&domain.BadActorRecord{}); err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
+	
 	return db
 }
--- internal/database/redis.go
+++ internal/database/redis.go
@@ -3,19 +3,34 @@ package database
 import (
 	"context"
 	"log"
-
+ "os"
 	"github.com/redis/go-redis/v9"
 )
 
 // NewRedisClient creates and returns a connected Redis client.
 func NewRedisClient() *redis.Client {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "host.docker.internal:6379",
-		Password: "",
-		DB:       0,
-	})
+	redisURL := os.Getenv("REDIS_URL")
+	var opt *redis.Options
+	var err error
 
-	_, err := client.Ping(context.Background()).Result()
+	if redisURL != "" {
+		// Cloud: Parse the secure Upstash URL
+		opt, err = redis.ParseURL(redisURL)
+		if err != nil {
+			log.Fatalf("Failed to parse Cloud Redis URL: %v", err)
+		}
+	} else {
+		// Local: Fallback to your exact Docker configuration
+		opt = &redis.Options{
+			Addr:     "host.docker.internal:6379",
+			Password: "",
+			DB:       0,
+		}
+	}
+
+	client := redis.NewClient(opt)
+
+	_, err = client.Ping(context.Background()).Result()
 	if err != nil {
 		log.Fatalf("Failed to connect to Redis: %v", err)
 	}
